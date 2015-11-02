b2fea4328aa5b13f039dce8ec0da73575819cf33
twillzy <william.tanzy@gmail.com>
2015-11-02 14:18:54 +1100

    done

--- server.js
+++ server.js
@@ -13,6 +13,58 @@ var send404 = function(response) {
 
 // Serves file data, writes appropriate HTTP header then sends the contents of the file
 var sendFile = function(response, filePath, fileContents) {
-	response.writeHead(200, {'Content-type': mime.lookup(path.base-name(filePath))});
+	response.writeHead(200, {'Content-type': mime.lookup(path.basename(filePath))});
 	response.end(fileContents);
 };
+
+// Cache frequently used data in memory
+var serveStatic = function(response, cache, absPath) {
+	if(cache[absPath]) {
+		sendFile(response, absPath, cache[absPath]);
+	} else {
+		fs.exists(absPath, function(exists) {
+			if (exists) {
+				fs.readFile(absPath, function(err, data) {
+					if (err) {
+						send404(response);
+					} else {
+						cache[absPath] = data;
+						sendFile(response, absPath, data);
+					}
+				});
+			} else {
+				send404(response);
+			}
+		});
+	}
+};
+
+// Create HTTP server
+var server = http.createServer(function(request, response) {
+	var filePath = false;
+
+	if(request.url === '/') {
+		filePath = 'public/index.html';
+	} else {
+		filePath = 'public' + request.url;
+	}
+
+	var absPath = './' + filePath;
+	serveStatic(response, cache, absPath);
+});
+
+server.listen(3000, function() {
+	console.log("Server listening on port 3000");
+});
+
+
+var chatServer = require('./lib/chat_server');
+chatServer.listen(server);
+
+
+
+
+
+
+
+
